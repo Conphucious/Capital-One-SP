8147706c31ee47ba22d9c8605b9eb1820df33592
Conphucious <9phuc.nguyen6@gmail.com>
2019-12-29 01:48:09 -0500

    Quick testing of old format and constants

--- src/main/java/com/ngu_software/capital_one_sp/App.java
+++ src/main/java/com/ngu_software/capital_one_sp/App.java
@@ -2,23 +2,54 @@ package com.ngu_software.capital_one_sp;
 
 import java.io.File;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.text.PDFTextStripper;
 
+// 01/31/2019 is after the new pdf formats for capital one 360
+
 public class App {
 	public static void main(String[] args) {
-		PDDocument document;
+		String text = processDocument(new File("test.pdf"));
+		
+		String date = text.split("Summary as of ")[1].split(" ")[0];
+		System.out.println(date);
+		Date d = createDate(date);
+		System.out.println(d);
+		
+		if (d.after(createDate(CO.NEW_FORMAT_DATE))) {
+			System.out.println("YUP");
+		}
+		
+	}
+	
+	private static String processDocument(File pdf) {
+		PDDocument document = null;
+		String text = null;
 		try {
-			document = PDDocument.load(new File("test.pdf"));
+			document = PDDocument.load(pdf);
 			if (!document.isEncrypted()) {
 			    PDFTextStripper stripper = new PDFTextStripper();
-			    String text = stripper.getText(document);
-			    System.out.println("Text:" + text);
+			    text = stripper.getText(document);
+//			    System.out.println("Text:" + text);
 			}
 			document.close();
 		} catch (IOException e) {
 			e.printStackTrace();
+		}
+		
+		return text;
+	}
+	
+	private static Date createDate(String date) {
+		try {
+			return new SimpleDateFormat("MM/dd/yyyy").parse(date);
+		} catch (ParseException e) {
+			e.printStackTrace();
+			return null;
 		}
 	}
 }
---
+++ src/main/java/com/ngu_software/capital_one_sp/CO.java
@@ -0,0 +1,7 @@
+package com.ngu_software.capital_one_sp;
+
+public class CO {
+	
+	public static final String NEW_FORMAT_DATE = "01/31/2019";
+
+}
