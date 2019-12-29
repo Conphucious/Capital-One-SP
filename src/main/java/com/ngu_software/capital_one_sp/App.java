9c0ba1baeeba9a25b5157844014d1ef05282718f
Conphucious <9phuc.nguyen6@gmail.com>
2019-12-29 01:36:31 -0500

    gitignore and testing PDD

--- src/main/java/com/ngu_software/capital_one_sp/App.java
+++ src/main/java/com/ngu_software/capital_one_sp/App.java
@@ -1,13 +1,24 @@
 package com.ngu_software.capital_one_sp;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.text.PDFTextStripper;
+
+public class App {
+	public static void main(String[] args) {
+		PDDocument document;
+		try {
+			document = PDDocument.load(new File("test.pdf"));
+			if (!document.isEncrypted()) {
+			    PDFTextStripper stripper = new PDFTextStripper();
+			    String text = stripper.getText(document);
+			    System.out.println("Text:" + text);
+			}
+			document.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
