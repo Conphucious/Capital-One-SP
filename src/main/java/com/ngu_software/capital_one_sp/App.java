695597884ce745d7bf90c541bfcb71f55683dc27
Conphucious <9phuc.nguyen6@gmail.com>
2019-12-29 02:10:36 -0500

    Valid parsing of both formats (old CO360 and new 2019 version)

--- src/main/java/com/ngu_software/capital_one_sp/App.java
+++ src/main/java/com/ngu_software/capital_one_sp/App.java
@@ -14,39 +14,39 @@ import org.apache.pdfbox.text.PDFTextStripper;
 public class App {
 	public static void main(String[] args) {
 		String text = processDocument(new File("test.pdf"));
+//		System.out.println(text);
 		
-		String date = text.split("Summary as of ")[1].split(" ")[0];
+		// TODO Need to use regex instead of all these regex splits
+		String date = text.contains(CO.IDENTIFIER_2014) ? text.split(CO.IDENTIFIER_2014)[1].split(" ")[0] : text.split(CO.IDENTIFIER_2019)[1].split("\n")[0].split("- ")[1];
 		System.out.println(date);
-		Date d = createDate(date);
+		Date d = createDate(date.replace(",", ""));
 		System.out.println(d);
-		
-		if (d.after(createDate(CO.NEW_FORMAT_DATE))) {
-			System.out.println("YUP");
-		}
-		
+
+		System.out.println(d.after(createDate(CO.NEW_FORMAT_DATE)) ? "Yes" : "No");
+
 	}
-	
+
 	private static String processDocument(File pdf) {
 		PDDocument document = null;
 		String text = null;
 		try {
 			document = PDDocument.load(pdf);
 			if (!document.isEncrypted()) {
-			    PDFTextStripper stripper = new PDFTextStripper();
-			    text = stripper.getText(document);
-//			    System.out.println("Text:" + text);
+				PDFTextStripper stripper = new PDFTextStripper();
+				text = stripper.getText(document);
 			}
 			document.close();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		
+
 		return text;
 	}
-	
+
 	private static Date createDate(String date) {
 		try {
-			return new SimpleDateFormat("MM/dd/yyyy").parse(date);
+			String format = date.contains("/") ? "MM/dd/yyyy" : "MMM dd yyyy";
+			return new SimpleDateFormat(format).parse(date);
 		} catch (ParseException e) {
 			e.printStackTrace();
 			return null;
--- src/main/java/com/ngu_software/capital_one_sp/CO.java
+++ src/main/java/com/ngu_software/capital_one_sp/CO.java
@@ -1,7 +1,12 @@
 package com.ngu_software.capital_one_sp;
 
 public class CO {
-	
+
 	public static final String NEW_FORMAT_DATE = "01/31/2019";
+
+	public static final String IDENTIFIER_2014 = "Summary as of ";
+	public static final String IDENTIFIER_2019 = "STATEMENT PERIOD  ";
+
+	private CO() {};
 
 }
