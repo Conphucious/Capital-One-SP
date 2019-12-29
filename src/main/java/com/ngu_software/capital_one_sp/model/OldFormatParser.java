95ae5c27d03cb36592488bddd034b24653d2c4d7
Conphucious <9phuc.nguyen6@gmail.com>
2019-12-29 14:11:33 -0500

    New regex for old pattern

--- src/main/java/com/ngu_software/capital_one_sp/model/OldFormatParser.java
+++ src/main/java/com/ngu_software/capital_one_sp/model/OldFormatParser.java
@@ -1,15 +1,26 @@
 package com.ngu_software.capital_one_sp.model;
 
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class OldFormatParser {
-	
+
+	private Pattern pattern = Pattern.compile("(?<=Opening Balance )(.*\n?)(?=Closing Balance )", Pattern.DOTALL);
+
 	public OldFormatParser(Date d, String text) {
 		Document doc = new Document(d);
-		String transText = text.split("Opening Balance ")[1].split("Go mobile and bank on the fly.")[0];
+//		String transText = text.split("Opening Balance ")[1].split("Go mobile and bank on the fly.")[0];
+		String transText = null;
+		Matcher m = pattern.matcher(text);
 		
-//		String tt = text.split("?<=beginningstringname)(.*\n?)(?=endstringname")[0];
-		System.out.println(transText);
+		while (m.find()) {
+			transText = text.substring(m.start(), m.end() - 1);
+			System.out.println(transText);
+		}
+
+//		String tt = text.split("?<=Opening Balance )(.*\n?)(?=Go mobile and bank on the fly.")[0];
+//		System.out.println(transText);
 	}
 
 }
