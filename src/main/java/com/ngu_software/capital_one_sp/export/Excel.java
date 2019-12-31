2302bb7938a0ef8bdaaf5a69007c30fde1121b7f
Conphucious <9phuc.nguyen6@gmail.com>
2019-12-30 21:55:52 -0500

    Test headers from CodeJava.com

--- src/main/java/com/ngu_software/capital_one_sp/export/Excel.java
+++ src/main/java/com/ngu_software/capital_one_sp/export/Excel.java
@@ -3,7 +3,10 @@ package com.ngu_software.capital_one_sp.export;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
+import org.apache.poi.sl.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
@@ -13,7 +16,11 @@ public class Excel {
 	public static void main(String[] args) throws IOException {
 		XSSFWorkbook workbook = new XSSFWorkbook();
 		XSSFSheet sheet = workbook.createSheet("Java Books");
-		
+
+		sheet.getWorkbook().createCellStyle();
+
+		createHeaderRow(sheet);
+
 		Object[][] bookData = { { "Head First Java", "Kathy Serria", 79 }, { "Effective Java", "Joshua Bloch", 36 },
 				{ "Clean Code", "Robert martin", 42 }, { "Thinking in Java", "Bruce Eckel", 35 }, };
 
@@ -38,6 +45,24 @@ public class Excel {
 		try (FileOutputStream outputStream = new FileOutputStream("JavaBooks.xlsx")) {
 			workbook.write(outputStream);
 		}
+	}
+
+	private static void createHeaderRow(XSSFSheet sheet) {
+		CellStyle cellStyle = sheet.getWorkbook().createCellStyle();
+		Font font = sheet.getWorkbook().createFont();
+		font.setBold(true);
+		font.setFontHeightInPoints((short) 16);
+		cellStyle.setFont(font);
+		Row row = sheet.createRow(0);
+		Cell cellTitle = row.createCell(1);
+		cellTitle.setCellStyle(cellStyle);
+		cellTitle.setCellValue("Title");
+		Cell cellAuthor = row.createCell(2);
+		cellAuthor.setCellStyle(cellStyle);
+		cellAuthor.setCellValue("Author");
+		Cell cellPrice = row.createCell(3);
+		cellPrice.setCellStyle(cellStyle);
+		cellPrice.setCellValue("Price");
 	}
 
 }
