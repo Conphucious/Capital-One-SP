028ade0d1fb3c915b591bdb078ba88c85c6ff262
Conphucious <9phuc.nguyen6@gmail.com>
2020-01-23 19:29:04 -0600

    Date parsing. Need year added.

--- src/main/java/com/ngu_software/capital_one_sp/parse/FormatParser2019.java
+++ src/main/java/com/ngu_software/capital_one_sp/parse/FormatParser2019.java
@@ -14,7 +14,7 @@ public class FormatParser2019 extends FormatParser {
 	private static final int DATE_END_CHAR_COUNT = 6;
 
 	private static Pattern pt = Pattern.compile("(?<=CYCLE)(.*\n?)(?=360 Checking)", Pattern.DOTALL);
-//	private static Pattern pd = Pattern.compile("\\b(\\d{2}\\/\\d{2}\\/\\d{4})");	// REGEX for MMM/dd/YYYY
+	private static Pattern pd = Pattern.compile("[a-zA-Z]{3} [0-9]{1,2}");	// REGEX for MMM/dd/YYYY
 //	private static Pattern pAct;
 //	private static Pattern pAmt = Pattern.compile("\\$(.*?)\\ ");
 
@@ -22,7 +22,7 @@ public class FormatParser2019 extends FormatParser {
 		Document doc = new Document(d);
 		String[] transText = findTransactions(text);
 
-//		System.out.println(text);
+		System.out.println(text);
 
 		// need to filter DATE DESCRIPTION CATEGORY AMOUNT BALANCE and extraneous stuff
 		// for pages
@@ -31,13 +31,18 @@ public class FormatParser2019 extends FormatParser {
 		for (int i = 0; i < transText.length; i++) {
 
 //			System.out.println(transText[i]);
-			if (isValidLine(transText[i])) {
-				String activity = getActivity(transText[i]);
+			
+			String activity = "";
+			
+			if (!isValidLine(transText[i])) {
+				continue;
 			}
+			
+			activity = getActivity(transText[i]);
+			Date date = getDate(transText[i]);
 			
 			// need to strip MMM dd and add on year from date d
 			
-//			Date date = getDate(transText[i]);
 //			String activity = getActivity(date, transText[i]);
 //			double amt = getAmount(transText[i]);
 //			Category category = getCategory(amt);
@@ -60,22 +65,17 @@ public class FormatParser2019 extends FormatParser {
 		List<String> lineList = new ArrayList<>();
 		
 		for (int i = 0; i < transText.length; i++) {
-			if (transText[i].contains("$") && !transText[i].contains("Opening Balance") && !transText[i].contains("Closing Balance")) {
+			if (transText[i].contains("$") && !transText[i].contains("Opening Balance") && !transText[i].contains("Closing Balance"))
 				lineList.add(transText[i]);
-			}
 		}
-
 		
 		return lineList.stream().toArray(String[] :: new);
 	}
 
 	private static boolean isValidLine(String text) {
 		if (text.length() > 6) {
-//			System.out.print(text.substring(0, 6));
-
 			Pattern pd = Pattern.compile("^([a-zA-Z]{3})(\\s{1})([0-9]{1,2})");
 			Matcher m = pd.matcher(text.substring(0, DATE_END_CHAR_COUNT).trim());
-
 			if (m.matches())
 				return true;
 		}
@@ -87,14 +87,17 @@ public class FormatParser2019 extends FormatParser {
 		return text.substring(6, text.length()).trim().split(" - \\$| \\+ \\$")[0];
 	}
 
-//	private static Date getDate(String text) {
-//		Matcher m = pd.matcher(text);
-//		
-//		while (m.find())
+	private static Date getDate(String text) {
+		Matcher m = pd.matcher(text);
+		System.out.println("< " + text);
+		while (m.find()) {
+			System.out.println("> " + text.substring(m.start(), m.end()));
+			return null;
+		}
 //			return createDate(text.substring(m.start(), m.end()));
-//		
-//		return null;
-//	}
+		
+		return null;
+	}
 //	
 //	private static String getActivity(Date date, String text) {
 //		pAct = Pattern.compile("([^" + new SimpleDateFormat("MM/dd/yyyy").format(date) + "]+)");
